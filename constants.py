c095ee0b14ecab0ea5b2bf842d42c44d1746d7b5
Traven Reese <82552048+Travenedu@users.noreply.github.com>
2021-10-24 18:15:07 -0400

    Game set up
    Added a frame rate and edited loop for the game. Also being testing drawing to the display.

--- constants.py
+++ constants.py
@@ -3,3 +3,5 @@ import pygame
 WIDTH, HEIGHT = 1200,800
 
 FPS = 60
+
+spaceship = pygame.transform.scale(pygame.image.load('assets/tiny_ship.png'), (88, 50))
--- game.py
+++ game.py
@@ -1,21 +1,36 @@
 import pygame
 pygame.init()
 
-from constants import WIDTH, HEIGHT
-
+from constants import WIDTH, HEIGHT, spaceship, FPS
 
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    screen.fill((0,0,0))
+#testing drawing
+def spaceship_draw():
+    screen.blit(spaceship, (370, 480))
 
-    pygame.draw.circle(screen, (0,0,255), (250,250), 75)
+#caption And Icon
+pygame.display.set_caption('Space Jump')
+pygame.display.set_icon(spaceship)
 
-    pygame.display.flip()
+def main():
+    running = True
+    clock = pygame.time.Clock()
 
-pygame.quit()
+    while running:
+        clock.tick(FPS)
+
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill((0,0,0))
+
+        spaceship_draw()
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+main()
