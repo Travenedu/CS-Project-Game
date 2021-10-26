49f3721cd1f120545a4118e41c7c40dc436ee84c
Traven Reese <82552048+Travenedu@users.noreply.github.com>
2021-10-26 10:37:34 -0400

    Small bug fix
    Fixed bug with images

--- constants.py
+++ constants.py
@@ -7,5 +7,5 @@ FPS = 60
 first_object = pygame.transform.scale(pygame.image.load('assets/tiny_ship.png'), (88, 50))
 first_objectX, first_objectY = 370, 20
 
-second_object = pygame.transform.scale(pygame.image.load('assets/second_object.png'), (88, 50))
+second_object = pygame.transform.scale(pygame.image.load('assets/Spaceman.png'), (88, 50))
 second_objectX, second_objectY = 370, 480
